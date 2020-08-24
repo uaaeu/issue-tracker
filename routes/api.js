a8a7cfca86a2608f918080df74f1df6d9f9ea6c6
Glitch (issue-tracker-uaaeu) <none>
2020-08-24 20:57:07 +0000

    issue schema and model created

--- routes/api.js
+++ routes/api.js
@@ -16,6 +16,20 @@ module.exports = function (app) {
 
   mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true});
   
+  let issueSchema = new mongoose.Schema({
+    issue_title: {type: String, required: true},
+    issue_text: {type: String, required: true},
+    created_by: {type: String, required: true},
+    assigned_to: String,
+    status_text: String,
+    open: {type: Boolean, required: true},
+    created_on: {type: Date, required: true},
+    updated_on: {type: Date, required: true},
+    project: String
+  });
+  
+  let Issue = mongoose.model('Issue', issueSchema);
+  
   app.route('/api/issues/:project')
   
     .get(function (req, res){
