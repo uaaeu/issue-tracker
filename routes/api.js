51f8aa26c97f08628157c3343b31fbe5db5a0d7e
Glitch (issue-tracker-uaaeu) <none>
2020-08-25 19:42:28 +0000

    post new issue and save to database

--- routes/api.js
+++ routes/api.js
@@ -39,7 +39,22 @@ module.exports = function (app) {
     
     .post(function (req, res){
       var project = req.params.project;
-      
+      let newIssue = new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || '',
+        open: true,
+        created_on: new Date().toUTCString(),
+        updated_on: new Date().toUTCString(),
+        project: project
+      })
+      newIssue.save((err, savedIssue)=>{
+        if(!err && savedIssue){
+          console.log(savedIssue)
+        }
+      })
     })
     
     .put(function (req, res){
